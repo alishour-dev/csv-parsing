20d478b15768cf1ecb817bd5fb4e9d4aeefdd023
Ali shour <iamshour@outlook.com>
2024-11-27 19:26:35 +0300

    fixed favicon

--- src/utils/baseMetadata.ts
+++ src/utils/baseMetadata.ts
@@ -7,31 +7,6 @@ import type { Metadata } from "next"
  */
 export const baseMetadata = ({ title, ...rest }: Metadata): Metadata => ({
 	description: "Tool created by Front-end Team!",
-	// icons: [
-	// 	{ rel: "icon", url: Favicon.src },
-	// 	{
-	// 		rel: "icon",
-	// 		sizes: "16x16",
-	// 		type: "image/png",
-	// 		url: Favicon16.src
-	// 	},
-	// 	{
-	// 		rel: "icon",
-	// 		sizes: "32x32",
-	// 		type: "image/png",
-	// 		url: Favicon32.src
-	// 	},
-	// 	{
-	// 		rel: "apple-touch-icon",
-	// 		sizes: "180x180",
-	// 		url: FaviconApple.src
-	// 	},
-	// 	{
-	// 		color: "#5bbad5",
-	// 		rel: "mask-icon",
-	// 		url: SafariPinnedTab
-	// 	}
-	// ],
 	other: { "msapplication-TileColor": "#da532c" },
 	title: {
 		default: (title || "CSV Parsing Tool") as string,
